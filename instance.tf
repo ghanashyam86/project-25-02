03cf462b8051da00bce4079b06cedb66858ac94f
dev <dev@gmail.com>
2025-02-25 13:03:46 +0000

    changes in region

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "public-instance" {
   instance_type           = "t2.micro"
   subnet_id               = aws_subnet.public_subnet[count.index].id
   vpc_security_group_ids  = [aws_security_group.vpc-demo-security-groups.id]
-  key_name                = "mumbai-key"
+  key_name                = "ohio-83"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   associate_public_ip_address = true
   tags = {
