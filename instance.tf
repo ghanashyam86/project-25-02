cd7e15e2f84556f3ff35bbe7d2d056e0add5d038
root <root@ip-172-31-0-201.us-east-2.compute.internal>
2025-02-25 12:57:29 +0000

    instance.tf file chages

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "public-instance" {
   instance_type           = "t2.micro"
   subnet_id               = aws_subnet.public_subnet[count.index].id
   vpc_security_group_ids  = [aws_security_group.vpc-demo-security-groups.id]
-  key_name                = "project-key"
+  key_name                = "mumbai-key"
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   associate_public_ip_address = true
   tags = {
